f2c99f6162fd93967d0642e50e08ff5ec27e0fb7
Matt Wilson <mattwwilson34@gmail.com>
2023-01-24 19:48:00 -0500

    Add: modularized functions and added comments

--- lyra-excel/app.py
+++ lyra-excel/app.py
@@ -1,83 +1,42 @@
 from openpyxl import load_workbook
-from pprint import pprint
 
 import utils
 
 workbook = load_workbook(filename='ATM.xlsx')
 workbook.sheetnames
 
-sheet_visit_1 = workbook[workbook.sheetnames[0]]
-sheet_visit_2_6 = workbook[workbook.sheetnames[1]]
+sheet_1 = workbook[workbook.sheetnames[0]]
+sheet_2 = workbook[workbook.sheetnames[1]]
 
-sheet_1_titles = []
-sheet_2_titles = []
-
-
-# Get all title from first sheet_visit_1
-for column in sheet_visit_1.iter_cols(min_row=1, max_row=1, min_col=1, max_col=50):
-    if column[0].value is None:
-        continue
-    else:
-        sheet_1_titles.append(column[0].value)
+sheet_1_titles = utils.get_row(sheet_1, 1)
+sheet_2_titles = utils.get_row(sheet_2, 1)
 
 
-for column in sheet_visit_2_6.iter_cols(min_row=1, max_row=1, min_col=1, max_col=50):
-    if column[0].value is None:
-        continue
-    else:
-        sheet_2_titles.append(column[0].value)
+l1 = utils.get_row(sheet_1, 1)
+l2 = utils.get_row(sheet_2, 1)
 
+l1Dict = utils.build_dictonary_from_cell_array(l1)
+l2Dict = utils.build_dictonary_from_cell_array(l2)
 
-def get_row_cells(sheet, row_number):
-    arr_of_cells = []
-    for row in sheet.iter_rows(min_row=row_number, max_row=row_number):
-        arr_of_cells.append(row)
-    return arr_of_cells
+# iterate over the smaller list of columns title cells
+for cell2 in l2:
+    # iterate over the larger list of colum title cells
+    for cell1 in l1:
+        # if the titles of the 2 title cells match append data from sheet 2
+        if cell1.value == cell2.value:
+            # get sheet 1 column number from dictionary
+            col_appending_to = l1Dict[cell1.value]
+            # get sheet 2 column number from dictionary
+            col_appending_from = l2Dict[cell1.value]
+            # get data cells array from sheet 2
+            data = utils.get_column_cells(
+                sheet_2, col_appending_from)
+            # append data to sheet 1
+            utils.append_data_to_sheet(
+                sheet_1, data, col_appending_to, 14)
 
+        # if there is not match of tiles then do nothing
+        else:
+            continue
 
-def get_column_cells(sheet, column_number):
-    arr_of_cells = []
-    for cells in sheet.iter_cols(min_col=column_number, max_col=column_number):
-        for cell in cells:
-            arr_of_cells.append(cell.value)
-    return arr_of_cells
-
-
-def get_column_depth(sheet):
-    for col_cells in sheet.iter_cols(min_row=1, min_col=1, max_col=1):
-        return len(col_cells)
-
-
-l1 = get_row_cells(sheet_visit_1, 1)[0]
-l2 = get_row_cells(sheet_visit_2_6, 1)[0]
-
-l1Dict = {}
-
-for cell in l1:
-    l1Dict[cell.value] = cell.column
-
-l2Dict = {}
-
-for cell in l2:
-    l2Dict[cell.value] = cell.column
-
-
-def append_data(sheet, data, col):
-    start = 14
-    for cell in data:
-        sheet.cell(row=start, column=col).value = cell
-        start += 1
-
-
-# for cell2 in l2:
-#     for cell1 in l1:
-#         if cell1.value == cell2.value:
-#             col_appending_to = l1Dict[cell1.value]
-#             col_of_data_appending = l2Dict[cell1.value]
-#             data = get_column_cells(sheet_visit_2_6, col_of_data_appending)
-#             append_data(sheet_visit_1, data, col_appending_to)
-#
-#         else:
-#             continue
-#
-# workbook.save(filename='ATM.xlsx')
+workbook.save(filename='ATM.xlsx')
--- lyra-excel/utils.py
+++ lyra-excel/utils.py
@@ -4,9 +4,38 @@ def print_row_cells(sheet, min, max):
 
 
 # gets an entire row of cells
+# A1,B1,C1,D1,...
 def get_row(sheet, row_number):
     cells = []
-    for col in sheet.iter_cols(max_row=1):
-        for cell in col:
+    for row in sheet.iter_cols(max_row=1):
+        for cell in row:
             cells.append(cell)
     return cells
+
+# gets an entire column of cells
+# A1,A2,A3,A4,...
+
+
+def get_column_cells(sheet, column_number):
+    arr_of_cells = []
+    for cells in sheet.iter_cols(min_col=column_number, max_col=column_number):
+        for cell in cells:
+            arr_of_cells.append(cell.value)
+    return arr_of_cells
+
+
+# build a dictionary from a list of cells
+# maping of dictionary is value ==> column number
+def build_dictonary_from_cell_array(cell_array):
+    cell_dict = {}
+    for cell in cell_array:
+        cell_dict[cell.value] = cell.column
+    return cell_dict
+
+
+# append an array of data cells, to a specified row and column in the provided
+# sheet
+def append_data_to_sheet(sheet, data_array, col_to_append_to, starting_row):
+    for cell in data_array:
+        sheet.cell(row=starting_row, column=col_to_append_to).value = cell
+        starting_row += 1
